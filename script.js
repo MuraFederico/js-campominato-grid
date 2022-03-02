1993ec9b53c57810ff9daf8d6fb77e4fef5532c9
MuraFederico <federicomura98@gmail.com>
2022-03-02 23:53:46 +0100

    aggiunto cambio colore sfondo on click e modificate le dimensione degli square in base al loro numero

--- script.js
+++ script.js
@@ -5,11 +5,43 @@ const btnPlay = document.querySelector('#play');
 btnPlay.addEventListener('click', function(){
     let difficulty = difficultyInput.value;
     if (difficulty == 'easy') {
+
         gridGenerator(100);
+
+        const square = document.querySelectorAll('.square');
+        
+        for (let index = 0; index < 100; index++) {
+            square[index].classList.add('small');
+            square[index].addEventListener('click', function() {
+                square[index].classList.toggle('bg-change');
+            })
+        }
+        
     } else if(difficulty == 'medium') {
+
         gridGenerator(81);
+
+        const square = document.querySelectorAll('.square');
+        
+        for (let index = 0; index < 81; index++) {
+            square[index].classList.add('medium');
+            square[index].addEventListener('click', function() {
+                square[index].classList.toggle('bg-change');
+            })
+        }
+
     } else {
+
         gridGenerator(49);
+
+        const square = document.querySelectorAll('.square');
+        
+        for (let index = 0; index < 49; index++) {
+            square[index].classList.add('large');
+            square[index].addEventListener('click', function() {
+                square[index].classList.toggle('bg-change');
+            })
+        }
     }
 })
 
@@ -18,13 +50,13 @@ function gridGenerator(squaresNumber) {
 
     container.innerHTML = '';
 
-
     for (let i = 1; i <= squaresNumber; i++) {
 
-        // container.innerHTML = `<div> ${i} </div>`
         const square = document.createElement('div');
+        square.classList.add('square')
         square.innerHTML = i;
         container.append(square);
         
     }
 }
+
