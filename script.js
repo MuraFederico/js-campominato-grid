5e2f37e5928026b1888701bb8d4753c982650e17
MuraFederico <federicomura98@gmail.com>
2022-03-03 14:04:19 +0100

    aggiunta generazione e assegnazione bombe

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ btnPlay.addEventListener('click', function(){
 
         const square = document.querySelectorAll('.square');
         
-        for (let index = 0; index < 100; index++) {
-            square[index].classList.add('small');
-            square[index].addEventListener('click', function() {
-                square[index].classList.toggle('bg-change');
-            })
-        }
+
+        // for (let index = 0; index < 100; index++) {
+        //     square[index].classList.add('small');
+        // }
+        square.forEach(square => square.classList.add('small'));
+
+        bombAssignment(bombGeneration(square.length -1));
         
     } else if(difficulty == 'medium') {
 
@@ -23,12 +24,12 @@ btnPlay.addEventListener('click', function(){
 
         const square = document.querySelectorAll('.square');
         
-        for (let index = 0; index < 81; index++) {
-            square[index].classList.add('medium');
-            square[index].addEventListener('click', function() {
-                square[index].classList.toggle('bg-change');
-            })
-        }
+        // for (let index = 0; index < 81; index++) {
+        //     square[index].classList.add('medium');
+        // }
+        square.forEach(square => square.classList.add('medium'));
+
+        bombAssignment(bombGeneration(square.length - 1));
 
     } else {
 
@@ -36,12 +37,12 @@ btnPlay.addEventListener('click', function(){
 
         const square = document.querySelectorAll('.square');
         
-        for (let index = 0; index < 49; index++) {
-            square[index].classList.add('large');
-            square[index].addEventListener('click', function() {
-                square[index].classList.toggle('bg-change');
-            })
-        }
+        // for (let index = 0; index < 49; index++) {
+        //     square[index].classList.add('large');
+        // }
+        square.forEach(square => square.classList.add('large'));
+
+        bombAssignment(bombGeneration(square.length - 1));
     }
 })
 
@@ -56,7 +57,35 @@ function gridGenerator(squaresNumber) {
         square.classList.add('square')
         square.innerHTML = i;
         container.append(square);
-        
+        square.addEventListener('click', squareSelection);
     }
 }
 
+function squareSelection() {
+    if(this.classList.contains('bomb')) {
+        this.classList.add('bg-change-bomb');
+    }else {
+        this.classList.add('bg-change-safe');
+    }
+}
+
+function bombAssignment(bombList) {
+    const square = document.querySelectorAll('.square');
+    for (let i = 0; i < bombList.length; i++) {
+        square[bombList[i]].classList.add('bomb')    
+    }
+}
+
+function bombGeneration(squaresNumber) {
+    const arrBombList = [];
+    for (i = 0; i < 16; i++) {
+        const random = Math.floor(Math.random() * squaresNumber + 1);
+        if(!arrBombList.includes(random)){
+            arrBombList.push(random);
+        }else{
+            i--;
+        }
+    }
+    console.log(arrBombList);
+    return arrBombList;
+}
